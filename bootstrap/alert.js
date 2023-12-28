9bfef97b9d608b280243a8ef4552c35825180a9b
Prashanth1698 <139166001+Prashanth1698@users.noreply.github.com>
2023-12-28 14:03:16 +0530

    Create alert.js

---
+++ bootstrap/alert.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Display an alert message
+  alert('Submitted successfully!');
+});
